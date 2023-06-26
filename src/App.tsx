151abd934644bcf71a3e34fa38b460d264653330
Renee Paczkowski <rpaczkowski@gmail.com>
2023-06-26 15:41:53 -0400

    Adjusted text shadow

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
 import React from "react";
 import logo from "./assets/logo.png";
 import "./App.scss";
-import ClassHours from "./components/class-hours/class-hours.component";
+// import ClassHours from "./components/class-hours/class-hours.component";
 
 const App = () => {
-  const classes = [
-    {
-      id: 1,
-      title: "1st Hour",
-    },
-    {
-      id: 2,
-      title: "2nd Hour",
-    },
-    {
-      id: 3,
-      title: "3rd Hour",
-    },
-    {
-      id: 4,
-      title: "4th Hour",
-    },
-    {
-      id: 5,
-      title: "5th Hour",
-    },
-    {
-      id: 6,
-      title: "6th Hour",
-    },
-    {
-      id: 7,
-      title: "7th Hour",
-    },
-  ];
+  // const classes = [
+  //   {
+  //     id: 1,
+  //     title: "1st Hour",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "2nd Hour",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "3rd Hour",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "4th Hour",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "5th Hour",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "6th Hour",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "7th Hour",
+  //   },
+  // ];
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div>
+      <div className="class-hours-container">
         <div className="class-hours">
           <h2>1st Hour</h2>
         </div>
--- src/types/class.types.ts
+++
@@ -1,3 +0,0 @@
-type ClassProps = [{id: number, title: string},];
-
-export default ClassProps;
