0d75d21518b869c03f570c4338a06cff48657188
Renee Paczkowski <rpaczkowski@gmail.com>
2023-06-26 14:11:12 -0400

    Building out additional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import logo from "./assets/logo.png";
 import "./App.scss";
-import ClassHours from "./components/class-hours.component";
+import ClassHours from "./components/class-hours/class-hours.component";
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <ClassHours />
-    </div>
-  );
-}
+const App = () => {
+  const classes = [
+    {
+      id: 1,
+      title: "1st Hour",
+    },
+    {
+      id: 2,
+      title: "2nd Hour",
+    },
+    {
+      id: 3,
+      title: "3rd Hour",
+    },
+    {
+      id: 4,
+      title: "4th Hour",
+    },
+    {
+      id: 5,
+      title: "5th Hour",
+    },
+    {
+      id: 6,
+      title: "6th Hour",
+    },
+    {
+      id: 7,
+      title: "7th Hour",
+    },
+  ];
+
+  return <Directory />;
+};
 
 export default App;
---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,13 @@
+import "../directory/directory.styles.scss";
+
+const Directory = ({ classes }) => {
+  return (
+    <div className="class-hours-container">
+      <div className="class-hours">
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
